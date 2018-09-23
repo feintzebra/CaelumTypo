da327386c6f897d0c93d840f157b3ffe3bbab55c
CaelumDev <walschot015@gmail.com>
2018-09-23 21:44:31 +0200

    up

---
+++ contracts.sol
@@ -0,0 +1,309 @@
+interface votingContract {
+    function getTokenProposalDetails() external view returns(address, uint, uint, uint);
+    function getExpiry() external view returns (uint);
+    function getContractType () external view returns (uint);
+}
+
+contract abstractCaelum {
+    function isMasternodeOwner(address _candidate) public view returns(bool);
+    function addToWhitelist(address _ad, uint _amount, uint daysAllowed) internal;
+    function addMasternode(address _candidate) internal returns(uint);
+    function deleteMasternode(uint entityAddress) internal returns(bool success);
+    function getLastPerUser(address _candidate) public view returns (uint);
+}
+
+contract NewMemberProposal is votingContract {
+
+    enum VOTE_TYPE {TOKEN, TEAM}
+    VOTE_TYPE public contractType = VOTE_TYPE.TEAM;
+
+    address memberAddress;
+    uint totalMasternodes;
+    uint votingDurationInDays;
+
+    /**
+     * @dev Create a new vote proposal for a team member.
+     * @param _contract Future team member's address
+     * @param _total How many masternodes do we want to give
+     * @param _voteDuration How many days is this vote available
+     */
+    constructor(address _contract, uint _total, uint _voteDuration) public {
+        require(_voteDuration >= 14 && _voteDuration <= 50, "Proposed voting duration does not meet requirements");
+        memberAddress = _contract;
+        totalMasternodes = _total;
+        votingDurationInDays = _voteDuration;
+    }
+
+    /**
+     * @dev Retuns all details about this proposal
+     */
+    function getTokenProposalDetails() public view returns(address, uint, uint, uint) {
+        return (memberAddress, totalMasternodes, 0, uint(contractType));
+    }
+
+    /**
+     * @dev Displays the expiry date of contract
+     * @return uint Days valid
+     */
+    function getExpiry() external view returns (uint) {
+        return votingDurationInDays;
+    }
+
+    /**
+     * @dev Displays the type of contract
+     * @return uint Enum value {TOKEN, TEAM}
+     */
+    function getContractType () external view returns (uint){
+        return uint(contractType);
+    }
+}
+
+contract NewTokenProposal is votingContract {
+
+    enum VOTE_TYPE {TOKEN, TEAM}
+
+    VOTE_TYPE public contractType = VOTE_TYPE.TOKEN;
+    address contractAddress;
+    uint requiredAmount;
+    uint validUntil;
+    uint votingDurationInDays;
+
+
+    /**
+     * @dev Create a new vote proposal for an ERC20 token.
+     * @param _contract ERC20 contract
+     * @param _amount How many tokens are required as collateral
+     * @param _valid How long do we accept these tokens on the contract (UNIX timestamp)
+     * @param _voteDuration How many days is this vote available
+     */
+    constructor(address _contract, uint _amount, uint _valid, uint _voteDuration) public {
+        require(_voteDuration >= 14 && _voteDuration <= 50, "Proposed voting duration does not meet requirements");
+
+        contractAddress = _contract;
+        requiredAmount = _amount;
+        validUntil = _valid;
+        votingDurationInDays = _voteDuration;
+    }
+
+    /**
+     * @dev Retuns all details about this proposal
+     */
+    function getTokenProposalDetails() public view returns(address, uint, uint, uint) {
+        return (contractAddress, requiredAmount, validUntil, uint(contractType));
+    }
+
+    /**
+     * @dev Displays the expiry date of contract
+     * @return uint Days valid
+     */
+    function getExpiry() external view returns (uint) {
+        return votingDurationInDays;
+    }
+
+    /**
+     * @dev Displays the type of contract
+     * @return uint Enum value {TOKEN, TEAM}
+     */
+    function getContractType () external view returns (uint){
+        return uint(contractType);
+    }
+}
+
+contract CaelumVotings {
+    using SafeMath for uint;
+
+    enum VOTE_TYPE {TOKEN, TEAM}
+
+    struct Proposals {
+        address tokenContract;
+        uint totalVotes;
+        uint proposedOn;
+        uint acceptedOn;
+        VOTE_TYPE proposalType;
+    }
+
+    uint PERCENTAGE_MAJORITY = 60;
+    uint MINIMUM_VOTERS_NEEDED = 5;
+    bool public proposalPending;
+
+    mapping(uint => Proposals) public proposalList;
+    uint public proposalCounter;
+
+    /**
+     * @notice Define abstract functions for later user
+     */
+    function isMasternodeOwner(address _candidate) public view returns(bool);
+    function addToWhitelist(address _ad, uint _amount, uint daysAllowed) internal;
+    function addMasternode(address _candidate) internal returns(uint);
+    function updateMasternodeAsTeamMember(address _member) internal returns (bool);
+    function isTeamMember (address _candidate) public view returns (bool);
+
+    /**
+     * @dev Create a new proposal.
+     * @param _contract Proposal contract address
+     * @return uint ProposalID
+     */
+    function pushProposal(address _contract) public returns (uint) {
+        if(proposalCounter != 0)
+        require (pastProposalTimeRules (), "You need to wait 90 days before submitting a new proposal.");
+        require (!proposalPending, "Another proposal is pending.");
+
+        uint _contractType = votingContract(_contract).getContractType();
+        proposalList[proposalCounter] = Proposals(_contract, 0, now, 0, VOTE_TYPE(_contractType));
+
+        proposalCounter++;
+        proposalPending = true;
+
+        return proposalCounter - 1;
+    }
+
+    /**
+     * @dev Internal function that handles the proposal after it got accepted.
+     * This function determines if the proposal is a token or team member proposal and executes the corresponding functions.
+     * @return uint Returns the proposal ID.
+     */
+    function handleLastProposal () internal returns (uint) {
+        uint _ID = proposalCounter.sub(1);
+
+        proposalList[_ID].acceptedOn = now;
+        proposalPending = false;
+
+        address _address;
+        uint _required;
+        uint _valid;
+        uint _type;
+        (_address, _required, _valid, _type) = getTokenProposalStuff(_ID);
+
+        if(_type == uint(VOTE_TYPE.TOKEN)) {
+            addToWhitelist(_address,_required,_valid);
+        }
+
+        if(_type == uint(VOTE_TYPE.TEAM)) {
+            if(_required != 0) {
+                for (uint i = 0; i < _required; i++) {
+                    addMasternode(_address);
+                }
+            } else {
+                addMasternode(_address);
+            }
+            updateMasternodeAsTeamMember(_address);
+        }
+
+        return _ID;
+    }
+
+    /**
+     * @dev Rejects the last proposal after the allowed voting time has expired and it's not accepted.
+     */
+    function discardRejectedProposal() public returns (bool) {
+        if (LastProposalCanDiscard())
+        proposalPending = false;
+        return (true);
+    }
+
+    /**
+     * @dev Checks if the last proposal allowed voting time has expired and it's not accepted.
+     * @return bool
+     */
+    function LastProposalCanDiscard () public view returns (bool) {
+        uint daysBeforeDiscard = votingContract(proposalList[proposalCounter - 1].tokenContract).getExpiry();
+        uint entryDate = proposalList[proposalCounter - 1].proposedOn;
+        uint expiryDate = entryDate + (daysBeforeDiscard * 1 days);
+
+        if (now >= expiryDate)
+        return true;
+    }
+
+    /**
+     * @dev Returns all details about a proposal
+     */
+    function getTokenProposalStuff(uint proposalID) public view returns(address, uint, uint, uint) {
+        return votingContract(proposalList[proposalID].tokenContract).getTokenProposalDetails();
+    }
+
+    /**
+     * @dev Returns if our 90 day cooldown has passed
+     * @return bool
+     */
+    function pastProposalTimeRules() public view returns (bool) {
+        uint lastProposal = proposalList[proposalCounter - 1].proposedOn;
+        if (now >= lastProposal + 90 days)
+        return true;
+    }
+
+
+    /**
+     * Voters section
+     */
+
+    struct Voters {
+        bool isVoter;
+        address owner;
+        uint[] votedFor;
+    }
+
+    mapping (address => Voters) public voterMap;
+    mapping(uint => address) public someStupid;
+    uint public votersCount;
+
+    /**
+     * @dev Allow any masternode user to become a voter.
+     */
+    function becomeVoter() public  {
+        require (isMasternodeOwner(msg.sender), "User has no masternodes");
+        require (!voterMap[msg.sender].isVoter, "User Already voted for this proposal");
+
+        voterMap[msg.sender].owner = msg.sender;
+        voterMap[msg.sender].isVoter = true;
+        votersCount = votersCount + 1;
+    }
+
+    /**
+     * @dev Allow voters to submit their vote on a proposal. Voters can only cast 1 vote per proposal.
+     * If the proposed vote is about adding Team members, only Team members are able to vote.
+     * A proposal can only be published if the total of votes is greater then MINIMUM_VOTERS_NEEDED.
+     * @param uint proposalID
+     */
+    function voteProposal(uint proposalID) public returns (bool success) {
+        require(voterMap[msg.sender].isVoter, "Sender not listed as voter");
+        require(proposalID >= 0, "No proposal was selected.");
+        require(proposalID <= proposalCounter, "Proposal out of limits.");
+        require(someStupid[proposalID] != msg.sender, "Already voted.");
+        require(votersCount >= MINIMUM_VOTERS_NEEDED, "Not enough voters in existence to push a proposal");
+
+        if(proposalList[proposalID].proposalType == VOTE_TYPE.TEAM) {
+            require (isTeamMember(msg.sender), "Restricted for team members");
+        }
+
+        someStupid[proposalID] = msg.sender;
+        proposalList[proposalID].totalVotes++;
+
+        if(reachedMajority(proposalID)) {
+            // This is the prefered way of handling vote results. It costs more gas but prevents tampering.
+            // If gas is an issue, you can comment handleLastProposal out and call it manually as onlyOwner.
+            handleLastProposal();
+            return true;
+        }
+    }
+
+    /**
+     * @dev Check if a proposal has reached the majority vote
+     * @param proposalID Token ID
+     * @return bool
+     */
+    function reachedMajority (uint proposalID) public view returns (bool) {
+        uint getProposalVotes = proposalList[proposalID].totalVotes;
+        if (getProposalVotes >= majority())
+        return true;
+    }
+
+    /**
+     * @dev Internal function that calculates the majority
+     * @return uint Total of votes needed for majority
+     */
+    function majority () internal view returns (uint) {
+        uint a = (votersCount * PERCENTAGE_MAJORITY );
+        return a / 100;
+    }
+
+}
