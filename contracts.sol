87db0d5bfe2bb4db9b7973a81a1f09bd83061fb0
CaelumDev <walschot015@gmail.com>
2018-09-23 22:00:17 +0200

    add acceptTokens

--- contracts.sol
+++ contracts.sol
@@ -1,3 +1,309 @@
+//solium-disable linebreak-style
+pragma solidity ^0.4.24;
+
+library ExtendedMath {
+    function limitLessThan(uint a, uint b) internal pure returns(uint c) {
+        if (a > b) return b;
+        return a;
+    }
+}
+
+library SafeMath {
+
+    /**
+     * @dev Multiplies two numbers, reverts on overflow.
+     */
+    function mul(uint256 _a, uint256 _b) internal pure returns(uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (_a == 0) {
+            return 0;
+        }
+
+        uint256 c = _a * _b;
+        require(c / _a == _b);
+
+        return c;
+    }
+
+    /**
+     * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
+     */
+    function div(uint256 _a, uint256 _b) internal pure returns(uint256) {
+        require(_b > 0); // Solidity only automatically asserts when dividing by 0
+        uint256 c = _a / _b;
+        // assert(_a == _b * c + _a % _b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+     */
+    function sub(uint256 _a, uint256 _b) internal pure returns(uint256) {
+        require(_b <= _a);
+        uint256 c = _a - _b;
+
+        return c;
+    }
+
+    /**
+     * @dev Adds two numbers, reverts on overflow.
+     */
+    function add(uint256 _a, uint256 _b) internal pure returns(uint256) {
+        uint256 c = _a + _b;
+        require(c >= _a);
+
+        return c;
+    }
+
+    /**
+     * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
+     * reverts when dividing by zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns(uint256) {
+        require(b != 0);
+        return a % b;
+    }
+}
+
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   * @notice Renouncing to ownership will leave the contract without an owner.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address _newOwner) public onlyOwner {
+    _transferOwnership(_newOwner);
+  }
+
+  /**
+   * @dev Transfers control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0));
+    emit OwnershipTransferred(owner, _newOwner);
+    owner = _newOwner;
+  }
+}
+
+contract ERC20Basic {
+    function totalSupply() public view returns(uint256);
+
+    function balanceOf(address _who) public view returns(uint256);
+
+    function transfer(address _to, uint256 _value) public returns(bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract ERC20 is ERC20Basic {
+    function allowance(address _owner, address _spender) public view returns(uint256);
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool);
+
+    function approve(address _spender, uint256 _value) public returns(bool);
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract BasicToken is ERC20Basic {
+    using SafeMath
+    for uint256;
+
+    mapping(address => uint256) internal balances;
+
+    uint256 internal totalSupply_;
+
+    /**
+     * @dev Total number of tokens in existence
+     */
+    function totalSupply() public view returns(uint256) {
+        return totalSupply_;
+    }
+
+    /**
+     * @dev Transfer token for a specified address
+     * @param _to The address to transfer to.
+     * @param _value The amount to be transferred.
+     */
+    function transfer(address _to, uint256 _value) public returns(bool) {
+        require(_value <= balances[msg.sender]);
+        require(_to != address(0));
+
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    /**
+     * @dev Gets the balance of the specified address.
+     * @param _owner The address to query the the balance of.
+     * @return An uint256 representing the amount owned by the passed address.
+     */
+    function balanceOf(address _owner) public view returns(uint256) {
+        return balances[_owner];
+    }
+
+}
+
+contract StandardToken is ERC20, BasicToken {
+
+    mapping(address => mapping(address => uint256)) internal allowed;
+
+
+    /**
+     * @dev Transfer tokens from one address to another
+     * @param _from address The address which you want to send tokens from
+     * @param _to address The address which you want to transfer to
+     * @param _value uint256 the amount of tokens to be transferred
+     */
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    )
+    public
+    returns(bool) {
+        require(_value <= balances[_from]);
+        require(_value <= allowed[_from][msg.sender]);
+        require(_to != address(0));
+
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param _spender The address which will spend the funds.
+     * @param _value The amount of tokens to be spent.
+     */
+    function approve(address _spender, uint256 _value) public returns(bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param _owner address The address which owns the funds.
+     * @param _spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(
+        address _owner,
+        address _spender
+    )
+    public
+    view
+    returns(uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    /**
+     * @dev Increase the amount of tokens that an owner allowed to a spender.
+     * approve should be called when allowed[_spender] == 0. To increment
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * @param _spender The address which will spend the funds.
+     * @param _addedValue The amount of tokens to increase the allowance by.
+     */
+    function increaseApproval(
+        address _spender,
+        uint256 _addedValue
+    )
+    public
+    returns(bool) {
+        allowed[msg.sender][_spender] = (
+            allowed[msg.sender][_spender].add(_addedValue));
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    /**
+     * @dev Decrease the amount of tokens that an owner allowed to a spender.
+     * approve should be called when allowed[_spender] == 0. To decrement
+     * allowed value is better to use this function to avoid 2 calls (and wait until
+     * the first transaction is mined)
+     * From MonolithDAO Token.sol
+     * @param _spender The address which will spend the funds.
+     * @param _subtractedValue The amount of tokens to decrease the allowance by.
+     */
+    function decreaseApproval(
+        address _spender,
+        uint256 _subtractedValue
+    )
+    public
+    returns(bool) {
+        uint256 oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue >= oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+}
+
+
+
+/**
+ * Begin Caelum
+ */
+
+
 interface votingContract {
     function getTokenProposalDetails() external view returns(address, uint, uint, uint);
     function getExpiry() external view returns (uint);
@@ -262,7 +568,7 @@ contract CaelumVotings {
      * @dev Allow voters to submit their vote on a proposal. Voters can only cast 1 vote per proposal.
      * If the proposed vote is about adding Team members, only Team members are able to vote.
      * A proposal can only be published if the total of votes is greater then MINIMUM_VOTERS_NEEDED.
-     * @param uint proposalID
+     * @param proposalID proposalID
      */
     function voteProposal(uint proposalID) public returns (bool success) {
         require(voterMap[msg.sender].isVoter, "Sender not listed as voter");
@@ -304,6 +610,143 @@ contract CaelumVotings {
     function majority () internal view returns (uint) {
         uint a = (votersCount * MAJORITY_PERCENTAGE_NEEDED );
         return a / 100;
+    }
+
+}
+
+
+contract CaelumAcceptERC20 is abstractCaelum {
+    using SafeMath for uint;
+
+    address[] public tokensList;
+    bool setOwnContract = true;
+
+    struct _whitelistTokens {
+        address tokenAddress;
+        bool active;
+        uint requiredAmount;
+        uint validUntil;
+        uint timestamp;
+    }
+
+    mapping(address => mapping(address => uint)) public tokens;
+    mapping(address => _whitelistTokens) acceptedTokens;
+
+    event Deposit(address token, address user, uint amount, uint balance);
+    event Withdraw(address token, address user, uint amount, uint balance);
+
+
+    /**
+     * @notice Allow the dev to set it's own token as accepted payment.
+     * @dev Can be hardcoded in the constructor. Given the contract size, we decided to separate it.
+     * @return bool
+     */
+    function addOwnToken() public returns (bool) {
+        require(setOwnContract);
+        addToWhitelist(this, 5000 * 1e8, 36500);
+        setOwnContract = false;
+        return true;
+    }
+
+    // TODO: Set visibility
+    /**
+     * @notice Add a new token as accepted payment method.
+     * @param _token Token contract address.
+     * @param _amount Required amount of this Token as collateral
+     * @param daysAllowed How many days will we accept this token?
+     */
+    function addToWhitelist(address _token, uint _amount, uint daysAllowed) internal {
+        _whitelistTokens storage newToken = acceptedTokens[_token];
+        newToken.tokenAddress = _token;
+        newToken.requiredAmount = _amount;
+        newToken.timestamp = now;
+        newToken.validUntil = now + (daysAllowed * 1 days);
+        newToken.active = true;
+
+        tokensList.push(_token);
+    }
+
+    /**
+     * @dev internal function to determine if we accept this token.
+     * @param _ad Token contract address
+     * @return bool
+     */
+    function isAcceptedToken(address _ad) internal view returns(bool) {
+        return acceptedTokens[_ad].active;
+    }
+
+    /**
+     * @dev internal function to determine the requiredAmount for a specific token.
+     * @param _ad Token contract address
+     * @return bool
+     */
+    function getAcceptedTokenAmount(address _ad) internal view returns(uint) {
+        return acceptedTokens[_ad].requiredAmount;
+    }
+
+    /**
+     * @dev internal function to determine if the token is still accepted timewise.
+     * @param _ad Token contract address
+     * @return bool
+     */
+    function isValid(address _ad) internal view returns(bool) {
+        uint endTime = acceptedTokens[_ad].validUntil;
+        if (block.timestamp < endTime) return true;
+        return false;
+    }
+
+    /**
+     * @notice Returns an array of all accepted token. You can get more details by calling getTokenDetails function with this address.
+     * @return array Address
+     */
+    function listAcceptedTokens() public view returns(address[]) {
+        return tokensList;
+    }
+
+    /**
+     * @notice Returns a full list of the token details
+     * @param token Token contract address
+     */
+    function getTokenDetails(address token) public view returns(address ad,uint required, bool active, uint valid) {
+        return (acceptedTokens[token].tokenAddress, acceptedTokens[token].requiredAmount,acceptedTokens[token].active, acceptedTokens[token].validUntil);
+    }
+
+    /**
+     * @notice Public function that allows any user to deposit accepted tokens as collateral to become a masternode.
+     * @param token Token contract address
+     * @param amount Amount to deposit
+     */
+    function depositCollateral(address token, uint amount) public {
+        require(isAcceptedToken(token), "ERC20 not authorised");  // Should be a token from our list
+        require(amount == getAcceptedTokenAmount(token));         // The amount needs to match our set amount
+        require(isValid(token));                                  // It should be called within the setup timeframe
+
+        tokens[token][msg.sender] = tokens[token][msg.sender].add(amount);
+
+        require(StandardToken(token).transferFrom(msg.sender, this, amount), "error with token");
+        emit Deposit(token, msg.sender, amount, tokens[token][msg.sender]);
+
+        addMasternode(msg.sender);
+    }
+
+    /**
+     * @notice Public function that allows any user to withdraw deposited tokens and stop as masternode
+     * @param token Token contract address
+     * @param amount Amount to withdraw
+     */
+    function withdrawCollateral(address token, uint amount) public {
+        require(token != 0); // token should be an actual address
+        require(isAcceptedToken(token), "ERC20 not authorised"); // Should be a token from our list
+        require(isMasternodeOwner(msg.sender)); // The sender must be a masternode prior to withdraw
+        require(tokens[token][msg.sender] == amount); // The amount must be exactly whatever is deposited
+
+        uint amountToWithdraw = tokens[token][msg.sender];
+        tokens[token][msg.sender] = 0;
+
+        deleteMasternode(getLastPerUser(msg.sender));
+
+        if (!StandardToken(token).transfer(msg.sender, amountToWithdraw)) revert();
+        emit Withdraw(token, msg.sender, amountToWithdraw, amountToWithdraw);
     }
 
 }
